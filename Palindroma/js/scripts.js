7e36eec71191157ca10f88f0018ef922cf7985b3
Paolo Rapuano <bettercallpaul07@gmail.com>
2023-01-11 07:34:39 +0100

    Completato

--- Palindroma/js/scripts.js
+++ Palindroma/js/scripts.js
@@ -1,1 +1,30 @@
+inputString = prompt('Scrivimi una parola, ti farò sapere se è palindroma!')
+
+
+function isPalindrome(inputString) {
+
+    // trasformiamo la parola tutta in lower case
+    let lower = inputString.toLowerCase();
+    
+    // convertiamo la parola in un array di caratteri
+    let characters = lower.split('');
+
+    // utilizziamo la funzione reverse per invertire l'array
+    let reversed = characters.reverse();
+
+    // uniamo nuovamente i caratteri in una parola
+    let reversedWord = reversed.join('');
+    
+    // confrontiamo la parola originale con quella invertita
+    if (lower === reversedWord) {
+        return "Si, la parola è palindroma";
+    }
+
+    else {
+        return "No, la parola non è palindroma"
+    }
+}
+
+alert(isPalindrome(inputString));
+
 
